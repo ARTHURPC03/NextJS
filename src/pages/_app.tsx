98d481dd9ebee5e5845934dc4988637aba5676b9
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-03 08:49:25 -0300

    Styled Components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,11 @@
+import GlobalStyle from "../styles/GlobalStyle";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 

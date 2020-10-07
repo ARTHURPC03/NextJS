cdbb2ef239c219d9474f1e485dcb4b84e314f386
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-07 07:13:25 -0300

    Utilizando import dinâmico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,12 @@ interface HomeProps {
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
+  async function handleSum() {
+    const math = (await import('../lib/math')).default
+
+    alert(math.sum(3,5))
+  }
+  
   return (
     <div>
       <section>
@@ -26,6 +32,8 @@ export default function Home({ recommendedProducts }: HomeProps) {
           })}
         </ul>
       </section>
+
+      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }

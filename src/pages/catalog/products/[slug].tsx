fb5c5f147b1cfe8ac04807254932f45d0e3852f1
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-07 19:21:17 -0300

    Paths no TypeScript

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import dynamic from 'next/dynamic'
 
 const AddToCartModal = dynamic(
-  () => import('../../../components/AddToCartModal'),
+  () => import('@/components/AddToCartModal'),
   { loading: () => <p>Loading...</p>, ssr: false } 
 )
 

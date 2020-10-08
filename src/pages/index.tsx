e78290121947a96ae1a5dae21e5d2965628267c5
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-08 06:23:30 -0300

    Utilizando SDK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 import { GetServerSideProps } from 'next'
+import Link from 'next/link'
 import { Title } from '@/styles/pages/Home'
 import SEO from '@/components/SEO'
-
-interface IProduct {
-  id: string
-  title: string
-}
+import { client } from '@/lib/prismic'
+import Prismic from 'prismic-javascript'
+import PrismicDOM from 'prismic-dom'
+import { Document } from 'prismic-javascript/types/documents'
 
 interface HomeProps {
-  recommendedProducts: IProduct[]
+  recommendedProducts: Document[]
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
-  async function handleSum() {
-    const math = (await import('../lib/math')).default
-
-    alert(math.sum(3,5))
-  }
   
   return (
     <div>
@@ -28,25 +23,31 @@ export default function Home({ recommendedProducts }: HomeProps) {
         <ul>
           {recommendedProducts.map(recommendedProduct => {
             return (
-              <li key={recommendedProduct.id} >
-                {recommendedProduct.title}
+              <li key={recommendedProduct.id}>
+                <Link href={`catalog/products/${recommendedProduct.uid}`} >
+                  <a>
+                  {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
               </li>
             )
           })}
         </ul>
       </section>
 
-      <button onClick={handleSum}>Sum!</button>
+      <button>Sum!</button>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`)
-  const recommendedProducts = await response.json()
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ])
+
   return {
     props: {
-      recommendedProducts
+      recommendedProducts: recommendedProducts.results
     }
   }
 }
